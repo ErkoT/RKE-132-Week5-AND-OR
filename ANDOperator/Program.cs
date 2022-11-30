e15351f6e7d1fc41d82e738edb99c8c08f20271a
Erko Targamaa <erkotargamaa@gmail.com>
2022-11-30 19:53:30 +0200

    week 5

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//math >=90; biology >=90; chemistry >=90
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math results: ");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology results: ");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry results: ");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if(math >= 90 && biology >= 90 && chemistry >=90)
+{
+    Console.WriteLine("Congratz! You got accepted");
+}
+else
+{
+    Console.WriteLine("Ohnooo, your score is too low");
+}
+
+
---
+++ OROperator/Program.cs
@@ -0,0 +1,21 @@
+﻿//math >=90; biology >=90; chemistry >=90
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math results: ");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology results: ");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry results: ");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >=90))
+{
+    Console.WriteLine("Congratz! You got accepted");
+}
+else
+{
+    Console.WriteLine("Ohnooo, your score is too low");
+}
